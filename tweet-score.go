48c108c0bad3f8a740d112e305ebe91cfd5d61e6
Andrew Nortrup <andrew.nortrup@gmail.com>
2015-08-12 20:00:07 -0700

    Refactored tweet-score into it's own file.

---
+++ tweet-score.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"time"
+
+	"golang.org/x/net/context"
+
+	"github.com/gorilla/feeds"
+)
+
+//TweetScore is a struct that shows the relative score of an address based on
+// it's populatrity
+type TweetScore struct {
+	Address    string
+	Score      int
+	LastActive time.Time
+	TweetIDs   []int64
+	Query      string
+}
+
+//GetFeedItem returns a feeds.Item to be inserted into an RSS or Atom feed
+func (score TweetScore) GetFeedItem(id string, c context.Context) *feeds.Item {
+	entry := &feeds.Item{
+		Link:    &feeds.Link{Href: score.Address},
+		Created: score.LastActive,
+		Id:      id,
+	}
+
+	return entry
+}
+
+func (score TweetScore) buildDescription(c context.Context) {
+	/*
+		var wg sync.WaitGroup
+		for tweetID := range score.TweetIDs {
+			wg.Add(1)
+			//Todo: Go routine to get the name of user and content of
+			//tweets and add add them to the description.
+		}
+	*/
+}
